51ec474a226100dab1d073ff51d9abebcf8c9293
Siegfried Boeken <37549326+VarVergErgos@users.noreply.github.com>
2023-01-22 10:55:57 +0100

    actualisize the Routing-Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
   },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'courses',
+    component: CoursesComponent,
+  },
+
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+
   {
     path: '**',
     redirectTo: '/',
